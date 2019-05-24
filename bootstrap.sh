ad2e55053fb92d4be52d28444e5f3972c176d23b
Pat Lambe <pat@maru.org.uk>
2019-05-24 14:27:04 +0100

    initial version

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,10 +1,14 @@
 #!/bin/sh
 
+USER_NAME=${USER_NAME:-admin}
+USER_PASSWD=${USER_PASSWD:-admin}
+USER_EMAIL=${USER_EMAIL:-${USER_NAME}@localhost.local}
+
 if [ ! -f "/data/vm/webvirtmgr.sqlite3" ]; then
 
 chown webvirtmgr /var/run/libvirt/libvirt-sock
 /usr/bin/python /webvirtmgr/manage.py syncdb --noinput
-echo "from django.contrib.auth.models import User; User.objects.create_superuser('admin', 'admin@localhost', '1234')" | /usr/bin/python /webvirtmgr/manage.py shell
+echo "from django.contrib.auth.models import User; User.objects.create_superuser('${USER_NAME}', '${USER_EMAIL}', '${USER_PASSWD}')" | /usr/bin/python /webvirtmgr/manage.py shell
 
 fi
 
